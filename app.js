cfed5da3278fe97a5c896bddfe3fac652b7c4856
Justas Gudonis <justasgudonis@Justass-MacBook-Pro.local>
2022-03-17 17:18:35 +0200

    some changes made

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello world again");
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.write("<h1>Yay it's weekend!</h1>");
+  } else {
+    res.sendFile(__dirname + "/index.html");
+  }
 });
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
