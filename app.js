b3839c2906790fe76b17cc84d9673e4c4bf0cedb
Justas Gudonis <justasgudonis@Justass-MacBook-Pro.local>
2022-03-28 20:35:13 +0300

    added day function

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
+  var day = "";
+  var week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-  if (currentDay === 6 || currentDay === 0) {
-    res.write("<h1>Yay it's weekend!</h1>");
+  day = week[currentDay];
+
+  /*   if (currentDay === 6 || currentDay === 0) {
+    day = "Weekend";
   } else {
-    res.sendFile(__dirname + "/index.html");
-  }
+    day = "Weekday";
+  } */
+
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
